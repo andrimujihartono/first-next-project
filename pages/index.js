da055f18723251d5eaef3c8fed1c1f7a771413bb
andrimuji69 <andrimuji69@gmail.com>
2022-06-17 23:58:47 +0700

    update title

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Andri Muji Hartono</title>
         <meta name="description" content="Andri Muji Hartono" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
